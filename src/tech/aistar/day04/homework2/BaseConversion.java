6d69e267cc3eec1dfeb51623b11dd71b07aada35
likuisuper <likuisuper@qq.com>
2020-07-25 16:17:43 +0800

    十进制转换为二进制

--- src/tech/aistar/day04/homework2/BaseConversion.java
+++ src/tech/aistar/day04/homework2/BaseConversion.java
@@ -12,7 +12,17 @@ package tech.aistar.day04.homework2;
  */
 public class BaseConversion {
     public static void main(String[] args) {
-        test(10);
+        //java.lang.Integer - [int类型对应包装类型]
+        //static String toBinaryString(int n);//jdk内置的十进制转二进制
+        //String binary = Integer.toBinaryString(10);
+        // System.out.println(binary);
+
+//        String binary = toBinary(10);
+//        System.out.println(binary);
+
+        //test(10);
+
+        System.out.println(toBinaryString(14));
     }
     public static void test(int n){
         if(n<0)
@@ -27,5 +37,47 @@ public class BaseConversion {
             sb.insert(0, sum);
         }
         System.out.println(sb);
+    }
+
+    /**
+     * 十进制 -> 二进制
+     * @param n
+     * @return
+     */
+    public static int toBinaryString(int n){
+
+
+        //思路 - 0 1 0 1
+
+        // 0*10的0次方 = 0
+        //1*10的1次方 = 10
+        //0*10的2次方 = 0
+        //1*10的3次方 = 1000
+
+        //相加 = 1 0 1 0
+
+        //定义一个指数的计数器
+        int pos = 0;
+
+        int binary = 0;//存储最终计算的二进制的数值
+
+        while(true){
+            //1. 得到商
+            int s = n / 2;
+            //2. 得到余数
+            int y = n % 2;
+
+            //TODO.. 待完成的功能....
+            binary+= y*Math.pow(10,pos++);
+
+            //3. 循环退出
+            if(s == 0)
+                break;
+
+            n = s;
+        }
+
+
+        return binary;
     }
 }
