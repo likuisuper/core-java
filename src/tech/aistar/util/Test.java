7c9eb89a5d33751eb7a51e99aede7e2fb7060c78
likuisuper <likuisuper@qq.com>
2020-07-25 19:01:12 +0800

    update

---
+++ src/tech/aistar/util/Test.java
@@ -0,0 +1,13 @@
+package tech.aistar.util;
+
+/**
+ * 本类功能:
+ *
+ * @author cxylk
+ * @date 2020/7/25 19:00
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(aaa);
+    }
+}
