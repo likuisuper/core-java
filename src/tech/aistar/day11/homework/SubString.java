d7d54fe71ddf6e869be8dd5e07c86bc080aa6ada
likuisuper <likuisuper@qq.com>
2020-08-05 10:41:10 +0800

    添加更新

--- src/tech/aistar/day11/homework/SubString.java
+++ src/tech/aistar/day11/homework/SubString.java
@@ -9,28 +9,58 @@ import java.util.Arrays;
  * @date 2020/8/4 19:11
  */
 public class SubString {
-    public static void main(String[] args) {
-        StringBuilder builder = new StringBuilder();
+    //数据的初始化 - 构造方法或者静态代码块
+    //private StringBuilder builder = new StringBuilder();
+
+    //    public StringYouErYuan(){
+//        builder.append("This is a log file,it was store at")
+//                .append("\"d:\\Program File\\temp\" ")
+//                .append("directory.the format of log file was:\n")
+//                .append("20130509:Jack:M:1990-08-08");
+//    }
+
+    //通过静态代码块进行初始化
+    private static StringBuilder builder = new StringBuilder();
+
+    static{
         builder.append("This is a log file,it was store at")
                 .append("\"d:\\Program File\\temp\" ")
                 .append("directory.the format of log file was:\n")
                 .append("20130509:Jack:M:1990-08-08");
+    }
 
-        //1.打印结果d:\Program File\temp
-        System.out.println(builder.indexOf(":"));
-        System.out.println(builder.indexOf("\" "));
-        System.out.print(builder.substring(36-1,55));
+    /**
+     * 1.截取此内容中的路径子字符串
+     *   打印出的结果为(找出包裹路径的双引号的下标,substring)
+     *   d:\Program File\temp
+     * @return
+     */
+    public static String test01(){
+        return builder.substring(builder.indexOf("\"")+1,
+                builder.lastIndexOf("\""));
+    }
 
-        System.out.println();
+    /**
+     * 2.把第一个引号之前的部分中的所有空格替换为#
+     *   把第二个引号之后的部分中的所有空格替换为*
+     * @return
+     */
+    public static String test02(){
+        //链式编程 - 可读性极差
+        return new StringBuilder()
+                .append(builder.substring(0,builder.indexOf("\""))
+                        .replace(" ","#"))
+                .append(builder.substring(builder.indexOf("\""),
+                        builder.lastIndexOf("\"")))
+                .append(builder.substring(builder.lastIndexOf("\""))
+                        .replace(" ","*")).toString();
+    }
 
-        //2.把第一个引号之前的部分中的所有空格替换为#
-        //  把第二个引号之后的部分中的所有空格替换为*
-        String str=builder.substring(0,36);
-        System.out.println(str.replace(" ","#"));
+    public static void main(String[] args) {
 
-        System.out.println(builder.indexOf(":",37));
-        String str1=builder.substring(93,builder.length());
-        System.out.println(str1.replace(" ","*"));
+        System.out.println(test01());
+
+        System.out.println(test02());
 
         //3.把\n后面的字符串取出打印
         System.out.println(builder.indexOf("\n"));
