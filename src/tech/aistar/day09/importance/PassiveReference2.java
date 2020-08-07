ec50a74f408cde533bf241f7dadbe7f8af4d3ae8
likuisuper <likuisuper@qq.com>
2020-08-07 19:45:45 +0800

    类的被动引用2

--- src/tech/aistar/day09/importance/PassiveReference2.java
+++ src/tech/aistar/day09/importance/PassiveReference2.java
@@ -1,10 +1,24 @@
 package tech.aistar.day09.importance;
 
 /**
- * 本类功能:
+ * 本类功能:被动使用类字段演示2
  *
  * @author cxylk
  * @date 2020/8/7 19:31
  */
+
+class SuperClass1{
+    static{
+        System.out.println("SuperClass1 init!");
+    }
+
+    public static int value=123;
+}
+
 public class PassiveReference2 {
+    public static void main(String[] args) {
+        //并没有输出 "SuperClass init",说明并没有触发类SuperClass的初始化阶段
+        //但是触发了另外一个名为Lorg.fenixsoft.class.SuperClass类的初始化
+        SuperClass1[] sca=new SuperClass1[10];
+    }
 }
---
+++ src/tech/aistar/day09/importance/PassiveReference3.java
@@ -0,0 +1,10 @@
+package tech.aistar.day09.importance;
+
+/**
+ * 本类功能:被动使用类字段演示3
+ *
+ * @author cxylk
+ * @date 2020/8/7 19:41
+ */
+public class PassiveReference3 {
+}
