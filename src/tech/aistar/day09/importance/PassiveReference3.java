0d7444e7d5ef386dc97b616ebe8a73604b345ea1
likuisuper <likuisuper@qq.com>
2020-08-07 19:50:54 +0800

    类的被动引用3

--- src/tech/aistar/day09/importance/PassiveReference3.java
+++ src/tech/aistar/day09/importance/PassiveReference3.java
@@ -6,5 +6,18 @@ package tech.aistar.day09.importance;
  * @author cxylk
  * @date 2020/8/7 19:41
  */
+
+class ConstClass{
+    static{
+        System.out.println("ConstClass init");
+    }
+    public static final String HELLOWORLD="hello word";
+}
 public class PassiveReference3 {
+    public static void main(String[] args) {
+        //并不会输出 ConstClass init
+        //因为编译阶段已经将此常量的值"hello world"存储到了NotInitialization类的
+        //常量池中
+        System.out.println(ConstClass.HELLOWORLD);
+    }
 }
