84b26db7301e91c4b3df78d223bd7df29e7612c5
likuisuper <likuisuper@qq.com>
2020-08-11 19:58:53 +0800

    删除非空文件夹

---
+++ src/tech/aistar/day15/homework/DelFolder.java
@@ -0,0 +1,31 @@
+package tech.aistar.day15.homework;
+
+import java.io.File;
+
+/**
+ * 本类功能:递归删除一个非空目录
+ *
+ * @author cxylk
+ * @date 2020/8/11 19:26
+ */
+public class DelFolder {
+    public static void main(String[] args) {
+        delFolder("E:\\QQGame");
+    }
+
+    public static void delFolder(String path){
+        File file=new File(path);
+        if(file.isDirectory()){
+            File[] files=file.listFiles();
+            for (File f : files) {
+                delFolder(f.getAbsolutePath());
+            }
+        }
+        //不管是不是文件夹都会被删除,当递归完,文件夹都为空
+        if(file.delete()){
+            System.out.println("删除成功");
+        }else
+            System.out.println("删除失败");
+
+    }
+}
