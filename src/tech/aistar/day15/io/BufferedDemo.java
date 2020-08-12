0aa2c2380c0b68c794bfd7db6b1c0c1fa25ce151
likuisuper <likuisuper@qq.com>
2020-08-12 18:39:09 +0800

    缓冲流

---
+++ src/tech/aistar/day15/io/BufferedDemo.java
@@ -0,0 +1,90 @@
+package tech.aistar.day15.io;
+
+import java.io.*;
+
+/**
+ * 本类功能:使用缓冲流
+ *
+ * @author cxylk
+ * @date 2020/8/12 18:07
+ */
+//@SuppressWarnings("all")
+public class BufferedDemo {
+    public static void main(String[] args) {
+        copy("src/tech/aistar/day15/FileDemo.java","src/tech/aistar/day15/FileDemo_副本3.txt");
+    }
+
+    /**
+     * 拷贝文件
+     * 语法糖写法 - 会自动关闭资源，不用再手动关闭
+     * @param src
+     * @param target
+     */
+    public static void copy(String src,String target){
+        try(BufferedInputStream in=new BufferedInputStream(new FileInputStream(src));
+            BufferedOutputStream out=new BufferedOutputStream(new FileOutputStream(target))){
+
+            int len=-1;
+            int count=0;
+            while(true){
+                //和不使用这个缓冲流的区别:
+                //底层默认初始化的这个缓冲数组8*1024 作用：面向磁盘的
+
+                //初始化BufferedInputStream 对象的时候同时初始化了这个数组,然后
+                //会把一定数量的数据加载到这个数组中
+
+                //然后执行read方法的时候,实际上是从底层的这个缓冲数组中读取内容的
+                //一旦底层的缓存数组中的数据被读取完毕之后,自动再接着讲磁盘下面的数据
+                //接着读取到底层的字节数组中
+                len=in.read();
+                count++;
+                if(len==-1)
+                    break;
+                out.write(len);
+            }
+
+            System.out.println("count:"+count);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+//    public static voie copy2(String src,String target){
+//        try(BufferedInputStream in=new BufferedInputStream(new FileInputStream(src));
+//        BufferedOutputStream out=new BufferedOutputStream(new FileOutputStream(target))) {
+//
+//            int len=-1;
+//            while(true){
+//                len=in.read();
+//                if(len==-1)
+//                    break;
+//                out.write(len);
+//            }
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    public static void copy3(String src,String target){
+//        try(BufferedInputStream in=new BufferedInputStream(new FileInputStream(src));
+//        BufferedOutputStream out=new BufferedOutputStream(new FileOutputStream(target))) {
+//
+//            int len=-1;
+//            while(true){
+//                len=in.read();
+//                if(len==-1)
+//                    break;
+//                out.write(len);
+//            }
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+}
---
+++ src/tech/aistar/day15/io/ObjectStreamDemo.java
@@ -0,0 +1,88 @@
+package tech.aistar.day15.io;
+
+import tech.aistar.day10.homework01.Book;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 本类功能:操作对象类型的文件的字节输入流/输出流
+ *
+ * @author cxylk
+ * @date 2020/8/12 17:46
+ */
+public class ObjectStreamDemo {
+    public static void main(String[] args) {
+        List<Book> books=new ArrayList<>();
+        Book b1=new Book(1,"1001","java");
+        Book b2=new Book(2,"1002","python");
+        Book b3=new Book(3,"1003","ai");
+
+        books.add(b1);
+        books.add(b2);
+        books.add(b3);
+
+        List<Book> list=findAll("src/tech/aistar/day15/io/books.txt");
+
+        if(null!=list&&list.size()>0){
+            list.forEach(e-> System.out.println(e));
+        }
+    }
+
+    /**
+     * 反序列化的过程
+     * @param path
+     * @return
+     */
+    public static List<Book> findAll(String path){
+        ObjectInputStream in=null;
+
+        List<Book> books=null;
+
+        try {
+            in=new ObjectInputStream(new FileInputStream(path));
+
+            books= (List<Book>) in.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }finally {
+            if(null!=in){
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return books;
+    }
+
+    /**
+     * 序列化的过程
+     * @param books
+     * @param path
+     */
+    public static void save(List<Book> books,String path){
+        //1.构建一个能够支持对象类型的文件字节输出流
+        ObjectOutputStream out=null;
+
+        try {
+            out=new ObjectOutputStream(new FileOutputStream(path));
+
+            out.writeObject(books);
+
+            System.out.println("保存集合成功");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            if(null!=out){
+                try {
+                    out.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
